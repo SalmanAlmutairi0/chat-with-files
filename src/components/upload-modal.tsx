946a31c724b82b8b775e7626029231d49b79e59b
salman almutairi <t123y2@gmail.com>
2025-01-26 02:04:06 +0300

    feat: enhance upload modal with file validation and toast notifications

--- src/components/upload-modal.tsx
+++ src/components/upload-modal.tsx
@@ -1,6 +1,6 @@
-"use clint";
+"use client";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -13,8 +13,63 @@ import { Button } from "./ui/button";
 import { Upload } from "lucide-react";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { useToast } from "@/hooks/use-toast";
 
 export default function UploadModal() {
+  const [file, setFile] = useState<File | null>(null);
+
+  const { toast } = useToast();
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleFileValidation = (file: File) => {
+    if (file.type !== "application/pdf") {
+      toast({
+        title: "خطأ",
+        description: " الرجاء التاكد ان صيغة الملف PDF",
+        variant: "destructive",
+      });
+      return false;
+    }
+
+    if (file.size > 4 * 1024 * 1024) {
+      toast({
+        title: "خطأ",
+        description: "الرجاء التاكد من حجم الملف لا يتجاوز 4 ميجا",
+        variant: "destructive",
+      });
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleFileUpload = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!file) {
+      toast({
+        title: "خطأ",
+        description: "الرجاء اختيار ملف",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    const isValid = handleFileValidation(file);
+
+    if (!isValid) {
+      console.log("file is not valid");
+      return;
+    }
+
+    console.log("file is valid");
+    // TODO: send the file to the server to be stored
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -25,17 +80,25 @@ export default function UploadModal() {
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="text-right mr-6">رفع الملف</DialogTitle>
-          {/* <DialogDescription className='text-right mr-6'>
-            اسحب الملفات هنا أو اضغط لتحميلها
-          </DialogDescription> */}
         </DialogHeader>
 
-        <form className="flex flex-col gap-4">
+        <form
+          className="flex flex-col gap-4"
+          onSubmit={(e) => handleFileUpload(e)}
+        >
           <div>
             <Label>اختر ملف</Label>
-            <Input type="file" placeholder="اختار ملف" />
+            <Input
+              type="file"
+              placeholder="اختار ملف"
+              onChange={(e) => handleFileChange(e)}
+              className="text-right border border-gray-500"
+            />
             <p className="text-[12px] mt-1 text-gray-500">
               * الرجاء التاكد ان صيغة الملف PDF
+            </p>
+            <p className="text-[12px] mt-1 text-gray-500">
+              * الرجاء التاكد من حجم الملف لا يتجاوز 4 ميجا
             </p>
           </div>
           <Button type="submit">رفع</Button>
