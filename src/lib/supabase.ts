ebd3818ed5c96df60fe633a4d07e81500c039164
salman almutairi <t123y2@gmail.com>
2025-01-26 21:16:27 +0300

    feat: add Supabase for database and storage

---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+// lib/supabaseServer.ts
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseServiceRoleKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabaseServer = createClient(supabaseUrl, supabaseServiceRoleKey);
