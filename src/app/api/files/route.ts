3688f5ac3b320557c67dbb54e7615d957605e97b
salman almutairi <t123y2@gmail.com>
2025-01-26 21:17:00 +0300

    feat: implement file upload API with Supabase storage

---
+++ src/app/api/files/route.ts
@@ -0,0 +1,110 @@
+import { toast } from "@/hooks/use-toast";
+import { supabaseServer } from "@/lib/supabase";
+import { NextResponse } from "next/server";
+
+type RequestData = {
+  file: File;
+};
+
+export const POST = async (req: Request, res: Response) => {
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file") as File;
+    const userid = formData.get("userId") as string;
+    console.log("User ID:", userid);
+
+    if (!userid) {
+      console.log("not authorized you have to login first");
+      return NextResponse.json({
+        status: 401,
+        message: "not authorized you have to login first",
+        toast: {
+          title: "خطأ",
+          description: "يجب تسجيل الدخول اولا",
+          variant: "destructive",
+        },
+      });
+    }
+
+    if (!file) {
+      console.log("No file received");
+      return NextResponse.json({
+        status: 404,
+        message: "No file received",
+        toast: {
+          title: "خطأ",
+          description: "الرجاء اختيار ملف",
+          variant: "destructive",
+        },
+      });
+    }
+
+    const fileBuffer = await file.arrayBuffer();
+    const bucketName = "files";
+
+    // seving file in supabase storage
+    const { data, error } = await supabaseServer.storage
+      .from(bucketName)
+      .upload(`public/${userid}/${file.name}`, fileBuffer, {
+        contentType: file.type,
+      });
+
+    if (error) {
+      console.error("Error uploading file to storage:", error);
+      return NextResponse.json({
+        status: 500,
+        message: "Error uploading file to storage",
+        toast: {
+          title: "خطأ",
+          description: "حدث خطأ اثناء رفع الملف",
+          variant: "destructive",
+        },
+      });
+    }
+
+    // Save the file metadata in the database
+    const { data: fileData, error: fileError } = await supabaseServer
+      .from("files")
+      .insert({
+        file_name: file.name,
+        file_path: data.fullPath,
+        file_format: file.type,
+        file_size: file.size,
+      });
+
+    if (fileError) {
+      console.error("Error saving file metadata:", fileError);
+      return NextResponse.json({
+        status: 500,
+        message: "Error saving file metadata",
+        toast: {
+          title: "خطأ",
+          description: "حدث خطأ اثناء رفع الملف",
+          variant: "destructive",
+        },
+      });
+    }
+
+    return NextResponse.json({
+      status: 200,
+      message: "File uploaded successfully",
+      data: fileData,
+      toast: {
+        title: "تم",
+        description: "تم رفع الملف بنجاح",
+        variant: "default",
+      },
+    });
+  } catch (error) {
+    console.error("Error handling file upload:", error);
+    return NextResponse.json({
+      status: 500,
+      message: "Error handling file upload",
+      toast: {
+        title: "خطأ",
+        description: "حدث خطأ اثناء رفع الملف",
+        variant: "destructive",
+      },
+    });
+  }
+};
