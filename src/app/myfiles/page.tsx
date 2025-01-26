d425fa8ba0743fdd571afb0dacf273ba978c4cea
salman almutairi <t123y2@gmail.com>
2025-01-26 22:40:48 +0300

    feat: enhance MyFiles page with FileCard and SkeletonCard components, and add loading state for uploads

--- src/app/myfiles/page.tsx
+++ src/app/myfiles/page.tsx
@@ -1,16 +1,26 @@
-import { Button } from '@/components/ui/button';
+"use client";
+import FileCard from '@/components/file-card';
+import SkeletonCard from '@/components/skeleton-card';
 import UploadModal from '@/components/upload-modal';
-import { Upload } from 'lucide-react';
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function MyFiles() {
-
+  const [isUploadLoading, setIsUploadLoading] = useState(false);
   return (
-    <div className="container mx-auto mt-10 ">
-      <div className="flex items-center justify-between">
+    <div className="container mx-auto flex flex-col space-y-24 mt-10 ">
+      
+      <div className="flex items-center justify-between px-2 md:px-4">
         <h1 className="text-3xl font-semibold ">مستنداتي</h1>
-        <UploadModal />
-       
+        <UploadModal isLoading={isUploadLoading} setIsLoading={setIsUploadLoading} />
+      </div>
+
+
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
+        {isUploadLoading && <SkeletonCard />}
+        <FileCard />
+        <FileCard />
+        <FileCard />
+        <FileCard />
       </div>
 
  
